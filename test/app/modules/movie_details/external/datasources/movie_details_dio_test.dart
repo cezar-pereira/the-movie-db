238480dd8e0233a56acd44104685a9584aaad7b5
cezar-pereira <helaniocezar@gmail.com>
2022-02-21 23:36:58 -0300

    refactored name

--- test/app/modules/movie_details/external/datasources/movie_details_dio_test.dart
+++ test/app/modules/movie_details/external/datasources/movie_details_dio_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:the_movie_db/app/modules/home/domain/errors/movie_failures.dart';
-import 'package:the_movie_db/app/modules/movie_details/infra/datasources/movie_datasource.dart';
+import 'package:the_movie_db/app/modules/movie_details/infra/datasources/movie_details_datasource.dart';
 import 'package:the_movie_db/app/modules/movie_details/infra/models/movie_details_model.dart';
 
 class MovieDetailsDatasourceMock extends Mock
